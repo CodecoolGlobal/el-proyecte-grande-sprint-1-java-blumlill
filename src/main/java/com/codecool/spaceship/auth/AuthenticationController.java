0983370d0c37eb780df82a4f2f0471beb71eff1b
silentstorm902 <jozsefkiss90@gmail.com>
2023-05-11 21:28:16 +0200

    added auth error handling

--- src/main/java/com/codecool/spaceship/auth/AuthenticationController.java
+++ src/main/java/com/codecool/spaceship/auth/AuthenticationController.java
@@ -1,5 +1,7 @@
 package com.codecool.spaceship.auth;
 
+import com.codecool.spaceship.model.UserEntity;
+import com.codecool.spaceship.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.http.ResponseEntity;
@@ -9,17 +11,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+    private final UserRepository userRepository;
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
         return ResponseEntity.ok(authenticationService.register(request));
     }
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
-        return ResponseEntity.ok(authenticationService.authenticate(request));
+    public ResponseEntity<String> authenticate(@RequestBody AuthenticationRequest request) {
+        AuthenticationResponse response = authenticationService.authenticate(request);
+        System.out.println(response.getToken());
+        if(response.getToken() != null) {
+            return ResponseEntity.status(201).body(response.toString());
+        } else {
+            Optional<UserEntity> user = userRepository.findByUsername(request.getUsername());
+            if(user == null) {
+                return ResponseEntity.status(403).body("invalid username");
+            } else {
+                return ResponseEntity.status(403).body("invalid password");
+            }
+        }
     }
 }
